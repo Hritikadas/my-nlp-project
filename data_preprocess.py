d81c91cd73fba587c753dff4d6219f8d665d3ec4
Hritikadas <hritikadas14@gmail.com>
2025-12-13 20:42:05 +0530

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""
-Created on 2020/4/15 21:01
-@author: phil
-"""
+
 
 import pandas as pd
 
@@ -17,3 +12,4 @@ def read_data(train_file="../dataset/kaggle-movie-review/train.tsv"):
 if __name__ == "__main__":
     X_data, y_data = read_data()
     print("train size", len(X_data))
+
