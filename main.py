e3a40e9805c8ea37978f1303d407820ca838505c
Hritikadas <hritikadas14@gmail.com>
2025-12-13 20:43:37 +0530

    Refactor main.py for clarity and structure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""
-Created on 2020/4/15 21:24
-@author: phil
-"""
+
 
 import numpy as np
 from data_preprocess import read_data
@@ -14,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     debug = 1
-    # 读入数据
+    
     X_data, y_data = read_data()
 
     if debug == 1:
@@ -26,7 +21,7 @@ if __name__ == '__main__':
         y_data = y_data[:1000]
     y = np.array(y_data).reshape(len(y_data), 1)
 
-    # 数据集划分
+    
     bag_of_word_model = BagOfWord(do_lower_case=True)
     ngram_model = NGram(ngram=(1, 2), do_lower_case=True)
     X_Bow = bag_of_word_model.fit_transform(X_data)
@@ -38,12 +33,12 @@ if __name__ == '__main__':
     X_train_Bow, X_test_Bow, y_train_Bow, y_test_Bow = train_test_split(X_Bow, y, test_size=0.2, random_state=42, stratify=y)
     X_train_Gram, X_test_Gram, y_train_Gram, y_test_Gram = train_test_split(X_Gram, y, test_size=0.2, random_state=42, stratify=y)
 
-    # 训练模型 不同特征的差别
+    
     epoch = 100
     bow_learning_rate = 1
     gram_learning_rate = 1
 
-    # 梯度下降
+    
     model1 = SoftmaxRegression()
     history = model1.fit(X_train_Bow, y_train_Bow, epoch=epoch, learning_rate=bow_learning_rate, print_loss_steps=epoch//10, update_strategy="stochastic")
     plt.plot(np.arange(len(history)), np.array(history))
@@ -56,9 +51,10 @@ if __name__ == '__main__':
     plt.show()
     print("Gram train {} test {}".format(model2.score(X_train_Gram, y_train_Gram), model2.score(X_test_Gram, y_test_Gram)))
 
-    # 样本数量：20000
+    
     # epoch = 100
     # bow_learning_rate = 0.001
     # gram_learning_rate = 0.5
     # Bow  train 0.7094375  test  0.4885
+
     # Gram train 0.9786875 test 0.5335
